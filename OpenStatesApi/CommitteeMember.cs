9bdcbdef70041205811c659864aade96f07204f3
Phillip Copley <phillip.copley@gmail.com>
2015-05-25 14:32:17 -0400

    Committee search and lookup logic with associated tests

---
+++ OpenStatesApi/CommitteeMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OpenStatesApi
+{
+    public class CommitteeMember
+    {
+        [JsonProperty( "leg_id" )]
+        public string LegislatorId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
